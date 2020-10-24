4f4bc30093b9372a4613a6b1935cf8aefabcf5d9
Davide Ghiotto <ghiotto.davidenko@gmail.com>
2020-10-24 13:00:09 +0200

    booking service + html basic

--- webapp/src/app/pages/book/book.component.ts
+++ webapp/src/app/pages/book/book.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { BookService } from '@services/book.service';
 
 @Component({
-  selector: 'app-book',
-  templateUrl: './book.component.html',
-  styleUrls: ['./book.component.sass']
+	selector: 'app-book',
+	templateUrl: './book.component.html',
+	styleUrls: ['./book.component.sass'],
 })
 export class BookComponent implements OnInit {
+	constructor(private book: BookService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
