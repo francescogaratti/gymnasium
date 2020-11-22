3c2a62849142dc78fff3890b18302486db15b087
Davide Ghiotto <ghiotto.davidenko@gmail.com>
2020-11-22 23:18:22 +0100

    fix workflow hosting + test

--- webapp/src/firebase-messaging-sw.js
+++ webapp/src/firebase-messaging-sw.js
@@ -21,16 +21,3 @@ firebase.initializeApp({
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
 const messaging = firebase.messaging();
-
-// messaging.onBackgroundMessage(function (payload) {
-// 	console.log('[firebase-messaging-sw.js] Received background message ', payload);
-// 	const notification = payload['notification'];
-// 	// Customize notification here
-// 	const notificationTitle = notification.title;
-// 	const notificationOptions = {
-// 		body: notification.body,
-// 		icon: notification.image,
-// 	};
-
-// 	self.registration.showNotification(notificationTitle, notificationOptions);
-// });
