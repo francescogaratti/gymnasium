8c369e71a38c91154431386d1f3eb6e739d4d761
Davide Ghiotto <ghiotto.davidenko@gmail.com>
2020-11-22 23:15:43 +0100

    fix workflows

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,17 +2,19 @@ import * as firebase from 'firebase/app';
 require('firebase/database');
 require('firebase/firestore');
 
-const firebaseConfig = {
-	apiKey: 'AIzaSyAX-eB7XUbfgRKxpq3y4nFnSkF2-iBv2Wk',
-	authDomain: 'ultra-gymnasium.firebaseapp.com',
-	databaseURL: 'https://ultra-gymnasium.firebaseio.com',
-	projectId: 'ultra-gymnasium',
-	storageBucket: 'ultra-gymnasium.appspot.com',
-	messagingSenderId: '13160087679',
-	appId: '1:13160087679:web:636c78361cf50f78773f4a',
-	measurementId: 'G-N3YW5GFN8D',
-};
-firebase.initializeApp(firebaseConfig);
+// const firebaseConfig = {
+// 	apiKey: 'AIzaSyAX-eB7XUbfgRKxpq3y4nFnSkF2-iBv2Wk',
+// 	authDomain: 'ultra-gymnasium.firebaseapp.com',
+// 	databaseURL: 'https://ultra-gymnasium.firebaseio.com',
+// 	projectId: 'ultra-gymnasium',
+// 	storageBucket: 'ultra-gymnasium.appspot.com',
+// 	messagingSenderId: '13160087679',
+// 	appId: '1:13160087679:web:636c78361cf50f78773f4a',
+// 	measurementId: 'G-N3YW5GFN8D',
+// };
+const firebaseConfig = process.env.FIREBASE_CONFIG;
+if (firebaseConfig) firebase.initializeApp(JSON.parse(firebaseConfig));
+else console.error('No config available');
 
 export * as excel from './create-excel';
 
--- webapp/src/environments/environment.ts
+++ webapp/src/environments/environment.ts
@@ -17,12 +17,3 @@ export const environment = {
 	cloudMessagingKeyPair:
 		'BJLjqKOOeBq3Owi3c3WaoGvQ999gkXnqAk53XdL0geWr2Fm2AbZjRKfZCg8_hRsO_03rolTplGT1BTMcULiHHLA',
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
