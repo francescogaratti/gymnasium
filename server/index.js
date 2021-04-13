66322023a54b3020a4b3798ea408fc2bf74e0434
Davide Ghiotto <ghiotto.davidenko@gmail.com>
2021-04-13 20:14:31 +0200

    changed port

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
 	res.send(`<h1>ultra-gymnasium Node.js server on heroku</h1>`);
 });
 
-app.listen(port, () => {
-	console.log(`ultra-gymnasium listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+	console.log(`ultra-gymnasium listening at http://localhost:${PORT}`);
 });
