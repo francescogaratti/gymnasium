742e1a5e3eff2873607dfc03f55f57fc4b70ca65
Davide Ghiotto <ghiotto.davidenko@gmail.com>
2021-04-13 20:17:15 +0200

    test api

--- server/index.js
+++ server/index.js
@@ -6,6 +6,20 @@ app.get('/', (req, res) => {
 	res.send(`<h1>ultra-gymnasium Node.js server on heroku</h1>`);
 });
 
+app.get('/workouts', (req, res) => {
+	let workouts = [
+		{
+			name: 'workout1',
+			date: '12/03/2021',
+		},
+		{
+			name: 'workout2',
+			date: '13/04/2021',
+		},
+	];
+	res.send(JSON.parse(workouts));
+});
+
 app.listen(PORT, () => {
 	console.log(`ultra-gymnasium listening at http://localhost:${PORT}`);
 });
