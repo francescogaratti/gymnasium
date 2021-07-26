9466a71d21584ce0789e23fe17b982092032e7ed
francescogaratti <francescogaratti97@gmail.com>
2021-07-26 23:11:52 +0200

    calcolo perc massimale

--- webapp/src/app/pages/extra-workout/extra-workout.component.ts
+++ webapp/src/app/pages/extra-workout/extra-workout.component.ts
@@ -1,12 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-import { ExerciseEntry, Exercise, ExerciseCategories, ExerciseType, mock } from '@models/exercise';
+import { MatNativeDateModule } from '@angular/material/core';
+import {
+	ExerciseEntry,
+	Exercise,
+	ExerciseCategories,
+	ExerciseType,
+	mock,
+	ExerciseRecord,
+} from '@models/exercise';
 import { User } from '@models/user';
 import { AuthService } from '@services/auth.service';
 import { UtilsService } from '@services/utils.service';
+import { flattenDiagnosticMessageText } from 'typescript';
 
-const mockExWeights = [55, 60, 65];
-
+const mockExWeights = [
+	{
+		name: 'panca',
+		weight: 50,
+		reps: 6,
+	},
+	{
+		name: 'leg curl',
+		weight: 80,
+		reps: 8,
+	},
+	{
+		name: 'tricipiti',
+		weight: 35,
+		reps: 10,
+	},
+];
 @Component({
 	selector: 'app-extra-workout',
 	templateUrl: './extra-workout.component.html',
@@ -16,7 +40,21 @@ export class ExtraWorkoutComponent implements OnInit {
 	user: User = null;
 	esercizi: Exercise[] = null;
 	selected_exercise: Exercise = null;
-	displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+	maxPercentage: number = null;
+	exesRecs = mockExWeights;
+
+	esercizioFormControl: FormControl = new FormControl('', [Validators.required]);
+
+	displayedColumns: string[] = [
+		'Max',
+		'2 reps',
+		'4 reps',
+		'6 reps',
+		'8 reps',
+		'10 reps',
+		'12 reps',
+		'15 reps',
+	];
 	dataSource = mockExWeights;
 
 	constructor() {}
@@ -26,5 +64,53 @@ export class ExtraWorkoutComponent implements OnInit {
 	selectExercise(exercise: Exercise) {
 		this.selected_exercise = exercise;
 		//this.workout_sessions = template.sessions;
+	}
+
+	changeExercise() {
+		this.selected_exercise = null;
+		this.esercizioFormControl.setValue(null);
+	}
+
+	repsToPerc(reps: number) {
+		let maxPercentage = null;
+		if (reps == 2 || reps == 3) {
+			maxPercentage = 90;
+		} else if (reps == 4 || reps == 5) {
+			maxPercentage = 85;
+		} else if (reps == 6 || reps == 7) {
+			maxPercentage = 80;
+		} else if (reps == 8 || reps == 9) {
+			maxPercentage = 75;
+		} else if (reps == 10 || reps == 11) {
+			maxPercentage = 70;
+		} else if (reps == 12) {
+			maxPercentage = 65;
+		} else if (reps == 15) {
+			maxPercentage = 60;
+		} else {
+			maxPercentage = 100;
+		}
+		return maxPercentage;
+	}
+
+	calculateMassimale(weight: number, reps: number) {
+		let perc = this.repsToPerc(reps);
+		let max = (weight * 100) / perc;
+		let percentuali = [60, 65, 70, 75, 80, 85, 90];
+		let valoricalcolati = [];
+
+		percentuali.forEach(p => {
+			//let v = (weight * p) / perc;
+			let v = (weight * p) / perc;
+			valoricalcolati.push(v);
+		});
+
+		console.info(valoricalcolati);
+		console.info(max);
+	}
+
+	logCose() {
+		console.log(this.repsToPerc(6));
+		//this.repsToPerc(mockExWeights[0].reps);
 	}
 }
