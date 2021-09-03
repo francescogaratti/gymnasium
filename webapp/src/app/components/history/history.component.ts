84688f124b6e2910697ff1f0ac69b9be34ac0948
Davide Ghiotto <ghiotto.davidenko@gmail.com>
2021-09-03 17:39:22 +0200

    completed history

--- webapp/src/app/components/history/history.component.ts
+++ webapp/src/app/components/history/history.component.ts
@@ -1,25 +1,15 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { SessionRecord } from '@models/workout';
 
-export interface PeriodicElement {
-	name: string;
-	position: number;
-	weight: number;
-	symbol: string;
+interface HistoryEntry {
+	exercise: string;
+	sets: number;
+	set1: number;
+	set2: number;
+	set3: number;
+	set4: number;
+	set5: number;
 }
-
-const ELEMENT_DATA: PeriodicElement[] = [
-	{ position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-	{ position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-	{ position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-	{ position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-	{ position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-	{ position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-	{ position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-	{ position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-	{ position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-	{ position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-];
 
 @Component({
 	selector: 'app-history',
@@ -28,10 +18,46 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class HistoryComponent implements OnInit {
 	@Input() sessionRecords: SessionRecord[];
-	displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-	dataSource = ELEMENT_DATA;
+	displayedColumns: string[] = ['exercise', 'set1', 'set2', 'set3', 'set4', 'set5'];
+	history: HistoryEntry[] = [];
+	history_index: number = 0;
+
+	MAX_NUMBER_OF_SETS = 5;
 
 	constructor() {}
 
-	ngOnInit(): void {}
+	ngOnInit(): void {
+		this.selectHistory();
+	}
+
+	nextHistory() {
+		this.history_index += 1;
+		this.selectHistory();
+	}
+
+	prevHistory() {
+		this.history_index -= 1;
+		this.selectHistory();
+	}
+
+	selectHistory() {
+		this.history = [];
+		this.sessionRecords[this.history_index].exercises.forEach(exercise => {
+			let weights = [];
+			let sets = exercise.weights.length;
+			for (let i = 0; i < sets; i++) weights.push(exercise.weights[i]);
+			// fill up if missing
+			for (let i = sets; i < this.MAX_NUMBER_OF_SETS; i++) weights.push(null);
+
+			this.history.push({
+				exercise: exercise.name,
+				sets: exercise.sets,
+				set1: weights[0],
+				set2: weights[1],
+				set3: weights[2],
+				set4: weights[3],
+				set5: weights[4],
+			});
+		});
+	}
 }
